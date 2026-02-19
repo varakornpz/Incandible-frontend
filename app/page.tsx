66d7c5a8c80fd150cae0f8ab71b1b284d45a81bd
Varakorn Kaewmanee <varakorn2324@gmail.com>
2026-02-19 20:41:37 +0700

    i spell this shit wrong

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Link href={"https://youtu.be/aZUXx6_GCC0?si=4MZW8TBxpfGJ63Cw"} className="bg-blue-500 w-fit h-fit px-2 py-1 rounded-md">Can tou test this button</Link>
+      <Link href={"https://youtu.be/aZUXx6_GCC0?si=4MZW8TBxpfGJ63Cw"} className="bg-blue-500 w-fit h-fit px-2 py-1 rounded-md">Can you test this button</Link>
     </div>
   );
 }

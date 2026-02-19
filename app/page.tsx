c6322827c9d7954c55a0443eb622820dbad1b02d
Varakorn Kaewmanee <varakorn2324@gmail.com>
2026-02-20 01:11:03 +0700

    add login page / og / favicon

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Link href={"https://youtu.be/aZUXx6_GCC0?si=4MZW8TBxpfGJ63Cw"} className="bg-blue-500 w-fit h-fit px-2 py-1 rounded-md">Can you test this button</Link>
+    <div className="w-full min-h-screen">
+      <div className="flex flex-col gap-y-6">
+        <div>
+          <Link className="bg-blue-500 text-xl rounded-xl px-2 py-1 font-semibold" href={"/signin"}>Sign in</Link>
+        </div>
+        <div>
+            <Link href={"https://youtu.be/aZUXx6_GCC0?si=4MZW8TBxpfGJ63Cw"} className="bg-blue-500 w-fit h-fit px-2 py-1 rounded-md">Can you test this button</Link>
+        
+        </div>
+      </div>
     </div>
   );
 }
---
+++ app/signin/page.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import Link from "next/link";
+
+
+import { FcGoogle } from "react-icons/fc";
+
+const loginUrl = "http://localhost:3334/auth/google/signin"
+
+export default ()=>{
+    return (
+        <div className="w-full min-h-screen flex flex-col items-center justify-center">
+            <div className="flex flex-col text-lg sm:text-xl items-center gap-y-2">
+                <p>
+                    Google sign in is the only way,
+                    <br className="md:hidden" /> 
+                    this is all I can do stop trying
+                </p>
+                <a href={loginUrl} className="flex items-center bg-white w-fit px-2 py-1 rounded-4xl border border-black/95">
+                    <FcGoogle className="w-10 h-10" />
+                    <p className="text-black text-xl font-normal">Sign in with Google</p>
+                </a>
+            </div>
+            <div className="pt-4">
+                <Link href="/" className="text-blue-500 underline">Back home</Link> 
+            </div>
+        </div>
+    )
+}

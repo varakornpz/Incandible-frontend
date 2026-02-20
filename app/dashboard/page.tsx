7498a3b45e96cd81de7983728cb7cf6219574ae1
Varakorn Kaewmanee <varakorn2324@gmail.com>
2026-02-20 21:21:36 +0700

    asdasd

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,31 +2,25 @@
 
 import axios from "axios"
 
-import { getCookie } from 'cookies-next/client'; 
 import { useEffect , useState } from "react";
 
 export default ()=>{
-    const [access_token ,setAccessToken] = useState<undefined | string>("")
     useEffect(()=>{
         const sayhi_api = process.env.NEXT_PUBLIC_API_SAY_HI
-        const token = getCookie("dump") as string | undefined
-        setAccessToken(token)
-        const fetchHi = async (api_url : string , t? : string) => {
+        const fetchHi = async (api_url : string) => {
             const res = await axios.get(api_url ,{
-                headers : {
-                    'Authorization': `Bearer ${t ?? ""}`
-                }
+                withCredentials : true
             })
             return res
         }
         if (sayhi_api) {
-            const res = fetchHi(sayhi_api , token)
+            const res = fetchHi(sayhi_api)
             console.log(res)
         }
     },[])
     return(
         <div>
-            <p className="text-white">{(access_token != undefined && access_token != "" )? access_token : "token not found"}</p>
+            <p className="text-white">Hello</p>
         </div>
     )
 }
